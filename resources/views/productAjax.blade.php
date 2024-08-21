52605cd0a61381c5c911a60d0c9d912eb94c509c
Tanveer177 <tanveerulhassan2018@gmail.com>
2024-08-21 16:29:48 +0500

    2nd commit

--- resources/views/productAjax.blade.php
+++ resources/views/productAjax.blade.php
@@ -3,20 +3,32 @@
 <head>
     <title>AJAX CRUD Operations In Laravel 10: Step-by-Step Guide - Vidvatek</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
-    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"/>
-    <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+
+    <!-- jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"/>
+
+    <!-- DataTables CSS -->
+    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+    <!-- jQuery Validation -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+
+    <!-- DataTables JS -->
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script> <!-- Ensure bundle.min.js is used for modal functionality -->
+
 </head>
 <body>
 
-<div class="container">
+<div class="container mt-5">
     <h1>AJAX CRUD Operations In Laravel 10: Step-by-Step Guide - Vidvatek</h1>
-    <a class="btn btn-info" href="javascript:void(0)" id="createNewProduct"> Add New Product</a>
+    <a class="btn btn-info mb-2" href="javascript:void(0)" id="createNewProduct">Add New Product</a>
     <table class="table table-bordered data-table">
         <thead>
             <tr>
@@ -31,15 +43,17 @@
     </table>
 </div>
 
+<!-- Modal -->
 <div class="modal fade" id="ajaxModelexa" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <h4 class="modal-title" id="modelHeading"></h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
             <div class="modal-body">
                 <form id="productForm" name="productForm" class="form-horizontal">
-                   <input type="hidden" name="id" id="id">
+                    <input type="hidden" name="id" id="id">
                     <div class="form-group">
                         <label for="title" class="col-sm-2 control-label">Title</label>
                         <div class="col-sm-12">
@@ -48,14 +62,14 @@
                     </div>
 
                     <div class="form-group">
-                        <label class="col-sm-2 control-label">Description</label>
+                        <label for="description" class="col-sm-2 control-label">Description</label>
                         <div class="col-sm-12">
                             <textarea id="description" name="description" required placeholder="Enter Description" class="form-control"></textarea>
                         </div>
                     </div>
 
                     <div class="col-sm-offset-2 col-sm-10">
-                     <button type="submit" class="btn btn-primary" id="savedata" value="create">Save Product</button>
+                        <button type="submit" class="btn btn-primary" id="savedata" value="create">Save Product</button>
                     </div>
                 </form>
             </div>
@@ -63,89 +77,90 @@
     </div>
 </div>
 
-</body>
-
 <script type="text/javascript">
-  $(function () {
+    $(function () {
 
-      $.ajaxSetup({
-          headers: {
-              'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-          }
-    });
-
-    var table = $('.data-table').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: "{{ route('products.index') }}",
-        columns: [
-            {data: 'DT_RowIndex', name: 'DT_RowIndex'},
-            {data: 'title', name: 'title'},
-            {data: 'description', name: 'description'},
-            {data: 'action', name: 'action', orderable: false, searchable: false},
-        ]
-    });
-
-    $('#createNewProduct').click(function () {
-        $('#savedata').val("create-product");
-        $('#id').val('');
-        $('#productForm').trigger("reset");
-        $('#modelHeading').html("Create New Product");
-        $('#ajaxModelexa').modal('show');
-    });
-
-    $('body').on('click', '.editProduct', function () {
-      var id = $(this).data('id');
-      $.get("{{ route('products.index') }}" +'/' + id +'/edit', function (data) {
-          $('#modelHeading').html("Edit Product");
-          $('#savedata').val("edit-user");
-          $('#ajaxModelexa').modal('show');
-          $('#id').val(data.id);
-          $('#title').val(data.title);
-          $('#description').val(data.description);
-      })
-   });
-
-    $('#savedata').click(function (e) {
-        e.preventDefault();
-        $(this).html('Sending..');
-
-        $.ajax({
-          data: $('#productForm').serialize(),
-          url: "{{ route('products.store') }}",
-          type: "POST",
-          dataType: 'json',
-          success: function (data) {
-
-              $('#productForm').trigger("reset");
-              $('#ajaxModelexa').modal('hide');
-              table.draw();
-
-          },
-          error: function (data) {
-              console.log('Error:', data);
-              $('#savedata').html('Save Changes');
-          }
-      });
-    });
-
-    $('body').on('click', '.deleteProduct', function () {
-
-        var id = $(this).data("id");
-        confirm("Are You sure want to delete this Product!");
-
-        $.ajax({
-            type: "DELETE",
-            url: "{{ route('products.store') }}"+'/'+id,
-            success: function (data) {
-                table.draw();
-            },
-            error: function (data) {
-                console.log('Error:', data);
+        // Setup CSRF token for AJAX requests
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
         });
-    });
 
-  });
+        // Initialize DataTables
+        var table = $('.data-table').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{ route('products.index') }}",
+            columns: [
+                { data: 'DT_RowIndex', name: 'DT_RowIndex' },
+                { data: 'title', name: 'title' },
+                { data: 'description', name: 'description' },
+                { data: 'action', name: 'action', orderable: false, searchable: false },
+            ]
+        });
+
+        // Show modal when 'Add New Product' is clicked
+        $('#createNewProduct').click(function () {
+            $('#id').val('');
+            $('#productForm').trigger("reset");
+            $('#modelHeading').html("Create New Product");
+            $('#ajaxModelexa').modal('show');
+        });
+
+        // Save product
+        $('#savedata').click(function (e) {
+            e.preventDefault();
+            $(this).html('Sending..');
+
+            $.ajax({
+                data: $('#productForm').serialize(),
+                url: "{{ route('products.store') }}",
+                type: "POST",
+                dataType: 'json',
+                success: function (data) {
+                    $('#productForm').trigger("reset");
+                    $('#ajaxModelexa').modal('hide');
+                    table.draw();
+                },
+                error: function (data) {
+                    console.log('Error:', data);
+                    $('#savedata').html('Save Changes');
+                }
+            });
+        });
+
+        // Edit product
+        $('body').on('click', '.editProduct', function () {
+            var id = $(this).data('id');
+            $.get("{{ route('products.index') }}" + '/' + id + '/edit', function (data) {
+                $('#modelHeading').html("Edit Product");
+                $('#savedata').val("edit-user");
+                $('#ajaxModelexa').modal('show');
+                $('#id').val(data.id);
+                $('#title').val(data.title);
+                $('#description').val(data.description);
+            });
+        });
+
+        // Delete product
+        $('body').on('click', '.deleteProduct', function () {
+            var id = $(this).data("id");
+            confirm("Are You sure want to delete this Product!");
+
+            $.ajax({
+                type: "DELETE",
+                url: "{{ route('products.store') }}" + '/' + id,
+                success: function (data) {
+                    table.draw();
+                },
+                error: function (data) {
+                    console.log('Error:', data);
+                }
+            });
+        });
+
+    });
 </script>
+</body>
 </html>
